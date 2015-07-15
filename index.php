b0b9c381b59f2603a20b7b81b24ed57bf50c8eda
gracevivar <grace_vb@hotmail.com>
2015-07-14 19:37:51 -0500

    conexion base

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
 
-echo 'Hola Mundo¡¡¡¡¡¡';
+$url = parse_url ( getenv (" CLEARDB_DATABASE_URL "));
+$server = $url [" host "];
+$username = $url [" user "];
+$password = $url [" pass "];
+$db = substr ( $url [" path "], 1);
+
+$conn = new mysqli ( $server , $username , $password , $db);
+
+echo "Server" . $server . '<br>';
+echo "Username" . $username . '<br>';
+echo "Password" . $password . '<br>';
+echo "DB" . $db . '<br>';

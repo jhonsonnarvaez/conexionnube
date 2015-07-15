76172467b055b038d637492627011e6b24cb71d6
gracevivar <grace_vb@hotmail.com>
2015-07-14 19:52:57 -0500

    conexion

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 
 $url = parse_url ( getenv (" CLEARDB_DATABASE_URL "));
-$server = $url [" us-cdbr-iron-east-02.cleardb.net"];
-$username = $url [" be505b883b85da "];
-$password = $url [" b22b5198 "];
-$db = substr ( $url [" heroku_a6c93f827357fde "], 1);
+$server = " us-cdbr-iron-east-02.cleardb.net";
+$username = " be505b883b85da ";
+$password = " b22b5198 ";
+$db = substr ( " heroku_a6c93f827357fde ", 1);
 
 $conn = new mysqli ( $server , $username , $password , $db);
 

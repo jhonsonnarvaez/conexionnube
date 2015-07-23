376539689520dddec94fc9d126fcbae33861c746
gracevivar <grace_vb@hotmail.com>
2015-07-22 21:52:09 -0500

    prueba

--- index.php
+++ index.php
@@ -1,63 +1,12 @@
-<?php
-
-$url = parse_url ( getenv (" CLEARDB_DATABASE_URL "));
-$server = $url [" host "];
-$username = $url [" user "];
-$password = $url [" pass "];
-$db = substr ( $url [" path "], 1);
-
-$conn = new mysqli ( $server , $username , $password , $db);
-
-echo "Server" . $server . '<br>';
-echo "Username" . $username . '<br>';
-echo "Password" . $password . '<br>';
-echo "DB" . $db . '<br>';
-
-if ($conn->connect_error)
-    die($conn->connect_error);
-else
-    echo 'Conexion exitosa' . '<br>' . '<br>';
-
-//Recibir el ide de colecciones
-
-$id = $_POST['id'];
-
-$query = "SELECT autor,titulo,tipo,anio FROM clasicos WHERE id='$id'";
-$res = $conn->query($query);
-$row = $res->fetch_assoc();
-$res->close();
-$conn->close();
-
-?>
-<html>
-
-    <head>
-        <meta charset="UTF-8">
-        <title>Modificar</title>
-    </head>
-    <body>
-        <form method="POST">
-            <input type="hidden" name="id" value="<?php echo $id; ?>">
-            <div>
-                <label for="autor"> Autor: </label>
-                <input type="text"  name="autor" value="<?php echo $row['autor']; ?>" id="autor"/>
-            </div>
-            <div>
-                <label for="titulo"> T&iacute;tulo: </label>
-                <input type="text"  name="titulo" value="<?php echo$row ['titulo']; ?>" id="titulo"/>
-            </div>
-            <div>
-                <label for="tipo"> Tipo: </label>
-                <input type="text"  name="tipo" value="<?php echo$row ['tipo']; ?>" id="tipo"/>
-            </div>
-            <div>
-                <label for="anio"> A&ntilde;o </label>
-                <input type="text"  name="anio" value="<?php echo$row ['anio']; ?>" id="anio"/>
-            </div>
-            <div>
-                <input type="submit" value="Guardar" name="Guardar" >
-            </div>
-        </form>
-    </body>
-</html>
-
+      <?php
+        error_reporting(0);
+//         $url = parse_url(getenv("CLEARDB_DATABASE_URL "));
+        $server = "us-cdbr-iron-east-02.cleardb.net";
+        $username = "b4464e370dc7e8";
+        $password = "0b6aa5f2";
+        $db = "heroku_6ec1694fce1e9a1";
+        $conn = new mysqli($server, $username, $password, $db);
+        if ($conn->connect_error)
+            die($conn->connect_error);
+        else
+           echo 'Conexion exitosa' . '<br>' . '<br>';
